c54271c2e6f36c3fcc1e08594f6f9592f4dbc48c
Ericka <64378078+eriimango@users.noreply.github.com>
2021-03-30 18:47:37 -0400

    Add files via upload

---
+++ digi_clock_main.py
@@ -0,0 +1,26 @@
+# hello im ericka bolta aka eriii mango and i will be implementing a gui digital clock in all pink interface.
+# This version will be a 12 hour format
+#author Ericka Bolta 3-30-21
+
+from tkinter import*
+from tkinter.ttk import*
+
+from time import strftime
+
+root = Tk()
+root.title("Pink DiGi Clock") #window title display
+
+def time():
+    string = strftime("%I:%M:%S %p") #12hour format
+    label.config(text=string)
+    label.after(1000, time)
+
+#imported a digital front from web and installed to my pc.
+label = Label(root, font=("ds-digital", 80), background= "pink", foreground= "hot pink")
+greeting = Label(text="by eriii mango", foreground="hot pink") #adding my tag
+label.pack(anchor="center")
+greeting.pack()
+time()
+
+mainloop()
+
